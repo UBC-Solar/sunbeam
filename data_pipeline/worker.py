1aa7799802165c9a7f15041b629776e5755ddec0
Joshua Riefman <joshuariefman@gmail.com>
2024-10-26 19:51:27 -0700

    Use logging + add overwrite

--- data_pipeline/worker.py
+++ data_pipeline/worker.py
@@ -1,6 +1,10 @@
 from influx_credentials import InfluxCredentials, INFLUXDB_CREDENTIAL_BLOCK_NAME
 from dotenv import load_dotenv
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler()])
+logger = logging.getLogger("PREFECT WORKER")
 
 
 def prepare_environment():
@@ -12,8 +16,8 @@ def prepare_environment():
     InfluxCredentials(
         influxdb_api_token=os.getenv("INFLUX_TOKEN"),
         influxdb_org=os.getenv("INFLUX_ORG")
-    ).save(INFLUXDB_CREDENTIAL_BLOCK_NAME)
-    print("Saving credentials...")
+    ).save(INFLUXDB_CREDENTIAL_BLOCK_NAME, overwrite=True)
+    logger.info("Saving credentials...")
 
 
 if __name__ == "__main__":
