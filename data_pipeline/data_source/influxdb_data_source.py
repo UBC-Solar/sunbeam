1294937ee3e18524c5fd1edae100077b86d07e65
Joshua Riefman <joshuariefman@gmail.com>
2024-11-19 15:36:37 -0800

    Update InfluxDB Data Source to select data for only a specific event

--- data_pipeline/data_source/influxdb_data_source.py
+++ data_pipeline/data_source/influxdb_data_source.py
@@ -1,9 +1,8 @@
-from datetime import datetime
-
 from data_tools.schema import File, Result
 from data_tools.schema import DataSource
 from data_tools.schema.data_source import FileLoader
 from data_tools.query import DBClient
+from data_tools.utils import parse_iso_datetime
 from data_pipeline.influx_credentials import InfluxCredentials, INFLUXDB_CREDENTIAL_BLOCK_NAME
 from datetime import datetime
 import os
@@ -36,12 +35,15 @@ class InfluxDBDataSource(DataSource):
                                   "as InfluxDB is read-only for Sunbeam!")
 
     def get(self, canonical_path: str, **kwargs) -> Result:
-        bucket, car, measurement, field = File.unwrap_canonical_path(canonical_path)
+        bucket, car, measurement, start, stop, field = File.unwrap_canonical_path(canonical_path)
+        start_dt: datetime = parse_iso_datetime(start)
+        stop_dt: datetime = parse_iso_datetime(stop)
+
         try:
             return Result.Ok(
                 self._influxdb_client.query_series(
-                    start=self._start,
-                    stop=self._stop,
+                    start=start_dt,
+                    stop=stop_dt,
                     bucket=bucket,
                     car=car,
                     measurement=measurement,
