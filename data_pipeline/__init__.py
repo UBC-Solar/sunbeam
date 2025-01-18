888ca5084ffd7b1f8e8943da291771813d1daec8
Joshua Riefman <joshuariefman@gmail.com>
2025-01-18 15:49:51 -0800

    Add Energy stage

--- data_pipeline/__init__.py
+++ data_pipeline/__init__.py
@@ -1,6 +1,6 @@
 from .logs import log_directory
 from .data_source import FSDataSource, InfluxDBDataSource, DataSourceFactory, DataSourceType
-from .stage import IngestStage, PowerStage, StageRegistry, Stage, StageResult, StageError, StageMeta
+from .stage import IngestStage, PowerStage, EnergyStage, StageRegistry, Stage, StageResult, StageError, StageMeta
 
 __all__ = [
     "log_directory",
@@ -10,6 +10,7 @@ __all__ = [
     "DataSourceType",
     "IngestStage",
     "PowerStage",
+    "EnergyStage",
     "StageRegistry",
     "Stage",
     "StageResult",
--- data_pipeline/stage/__init__.py
+++ data_pipeline/stage/__init__.py
@@ -1,11 +1,13 @@
 from .ingest_stage import IngestStage
 from .power_stage import PowerStage
+from .energy_stage import EnergyStage
 from .stage_registry import StageRegistry
 from .stage import Stage, StageResult, StageError, StageMeta
 
 __all__ = [
     "IngestStage",
     "PowerStage",
+    "EnergyStage",
     "StageRegistry",
     "Stage",
     "StageResult",
