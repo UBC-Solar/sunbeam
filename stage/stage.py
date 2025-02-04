3ab527ef16e5a37adfd3cc777a8b9292f465c7db
Joshua Riefman <joshuariefman@gmail.com>
2025-02-04 00:24:36 -0800

    Name task annotations

--- stage/stage.py
+++ stage/stage.py
@@ -231,9 +231,9 @@ class Stage(ABC, metaclass=StageMeta):
     @abstractmethod
     def run(self, *args) -> StageResult:
         # Here, we are annotating the stage functions at runtime as a Prefect task, then calling them
-        extract = task(self.extract)(*args)
-        transform = task(self.transform)(*extract)
-        load = task(self.load)(*transform)
+        extract = task(self.extract, f"{self.get_stage_name()} Extract")(*args)
+        transform = task(self.transform, f"{self.get_stage_name()} Transform")(*extract)
+        load = task(self.load, f"{self.get_stage_name()} Load")(*transform)
 
         return StageResult(self, *load)
 
