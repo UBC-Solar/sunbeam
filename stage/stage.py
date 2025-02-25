84be366c20c4a55a280c1a3083cf922a4418a2bf
Joshua Riefman <joshuariefman@gmail.com>
2025-02-24 16:26:07 -0800

    Add Stage static data loading

--- stage/stage.py
+++ stage/stage.py
@@ -1,12 +1,18 @@
-from abc import ABC, abstractmethod, ABCMeta
-import logging
-
 from data_tools.schema import FileLoader, Result
 from stage.stage_registry import stage_registry
-from stage.context import Context
+from abc import ABC, abstractmethod, ABCMeta
 from collections.abc import Iterable
-from prefect import task
+from stage.context import Context
 from logs import SunbeamLogger
+from typing import Callable
+from prefect import task
+from pathlib import Path
+import toml as tomllib
+import numpy as np
+import logging
+import json
+import dill
+import os
 
 
 class StageError(RuntimeError):
@@ -46,11 +52,72 @@ class Stage(ABC, metaclass=StageMeta):
         return super().__new__(cls)
 
     def __init__(self, *args, **kwargs):
+        """
+        Initialize the Stage base class.
+
+        Optionally, for controlling which directories are loaded as static stage data, you may set `data_pattern` to
+        a Callable which receives one `str` argument (the directory name) and returns a `bool` where True/False means
+        Include/Exclude.
+        """
         self._finalized = False
 
         self._context = Context.get_instance()
         self._logger = SunbeamLogger(self.__class__.get_stage_name())
-        self._expected_outputs = []
+
+        # Acquire stage data
+        self._stage_data = {}
+        self._fetch_data(kwargs.get("data_pattern"))
+
+    def _fetch_data(self, predicate: Callable[[str], bool] = None):
+        # If we haven't specified a predicate, allow all.
+        if predicate is None:
+            predicate = lambda _: True
+
+        stage_data_path = Path(__file__).parent / self.get_stage_name()
+
+        def load_item(dest: dict, path: Path):
+            # If the path is to another directory, we want to load the items in that directory
+            if os.path.isdir(path):
+                # Set up a new dictionary for the subdirectory
+                directory_name = os.path.split(path)[1]  # Get the tail
+
+                # If the predicate fails, we want to skip this directory
+                if not predicate(directory_name):
+                    return
+
+                dest[directory_name] = {}
+
+                # Load each subitem into the subdirectory
+                for item in os.listdir(path):
+                    load_item(dest[directory_name], path / item)
+
+            # The path is to a file. We want to load the file's contents.
+            else:
+                file_name, file_extension = os.path.splitext(os.path.split(path)[1])
+
+                if file_extension.lower() == ".json":
+                    with open(path, "r") as f:
+                        dest[file_name] = json.load(f)
+
+                if file_extension.lower() == ".toml":
+                    with open(path, "r") as f:
+                        dest[file_name] = tomllib.load(f)
+
+                if file_extension.lower() == ".pkl" or file_extension.lower() == ".pickle":
+                    with open(path, "rb") as f:
+                        dest[file_name] = dill.load(f)
+
+                if file_extension.lower() == ".npy":
+                    with open(path, "rb") as f:
+                        dest[file_name] = np.load(f)
+
+        if os.path.isdir(stage_data_path):
+            stage_data_items = os.listdir(stage_data_path)
+            for stage_data_item in stage_data_items:
+                load_item(self._stage_data, stage_data_path / stage_data_item)
+
+        else:
+            self.logger.info(f"Did not find any stage data for {self.get_stage_name()}!")
 
     def __setattr__(self, key, value):
         if hasattr(self, "_finalized"):
@@ -103,3 +170,7 @@ class Stage(ABC, metaclass=StageMeta):
     @property
     def context(self) -> Context:
         return self._context
+
+    @property
+    def stage_data(self) -> dict:
+        return self._stage_data
