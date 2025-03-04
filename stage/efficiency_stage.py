cdd70b2d64b62b9f07160245765e518e564fdf53
Jonah Lee <jonahjacqueslee@gmail.com>
2025-03-03 19:03:31 -0800

    add integrated efficiency

--- stage/efficiency_stage.py
+++ stage/efficiency_stage.py
@@ -4,6 +4,7 @@ from stage.stage_registry import stage_registry
 from data_tools.schema import Result, UnwrappedError, File, FileType, CanonicalPath
 from data_tools.collections import TimeSeries
 from prefect import task
+import numpy as np
 
 
 class EfficiencyStage(Stage):
@@ -24,7 +25,7 @@ class EfficiencyStage(Stage):
         :param self: an instance of EfficiencyStage to be run
         :param FileLoader vehicle_velocity_loader: loader to VehicleVelocity from Ingest
         :param FileLoader motor_power_loader: loader to Motor Power from PowerStage
-        :returns: InstantaneousEfficiency (FileLoader pointing to TimeSeries)
+        :returns: InstantaneousEfficiency, IntegratedEfficiency (FileLoaders pointing to TimeSeries)
         """
         return super().run(self, vehicle_velocity_loader, motor_power_loader)
 
@@ -46,23 +47,35 @@ class EfficiencyStage(Stage):
 
         return vehicle_velocity_result, motor_power_result
 
-    def transform(self, vehicle_velocity_result, motor_power_result) -> tuple[Result]:
+    def transform(self, vehicle_velocity_result, motor_power_result) -> tuple[Result, Result]:
         try:
             vehicle_velocity_ts: TimeSeries = vehicle_velocity_result.unwrap().data
             motor_power_ts: TimeSeries = motor_power_result.unwrap().data
             vehicle_velocity_aligned, motor_power_aligned = TimeSeries.align(
                 vehicle_velocity_ts, motor_power_ts)
-            instantaneous_efficiency = vehicle_velocity_aligned.promote(vehicle_velocity_aligned / motor_power_aligned)
-            instantaneous_efficiency.units = "J/m"
-            instantaneous_efficiency.name = "Instantaneous Efficiency"
-            instantaneous_efficiency_result = Result.Ok(instantaneous_efficiency)
+            instantaneous_efficiency_ts = vehicle_velocity_aligned.promote(vehicle_velocity_aligned / motor_power_aligned)
+            instantaneous_efficiency_ts.units = "J/m"
+            instantaneous_efficiency_ts.name = "Instantaneous Efficiency"
+            instantaneous_efficiency_result = Result.Ok(instantaneous_efficiency_ts)
         except UnwrappedError as e:
             self.logger.error(f"Failed to unwrap result! \n {e}")
             instantaneous_efficiency_result = Result.Err(RuntimeError("Failed to process instantaneous efficiency!"))
 
-        return (instantaneous_efficiency_result,)
+        try:
+            instantaneous_efficiency_ts: TimeSeries = instantaneous_efficiency_result.unwrap().data
+            integrated_efficiency_ts: TimeSeries = instantaneous_efficiency_ts.promote(
+                np.cumsum(instantaneous_efficiency_ts)
+            )
+            integrated_efficiency_ts.units = "J/m"
+            integrated_efficiency_ts.name = "Integrated Efficiency"
+            integrated_efficiency_result = Result.Ok(integrated_efficiency_ts)
+        except UnwrappedError as e:
+            self.logger.error(f"Failed to unwrap result! \n {e}")
+            integrated_efficiency_result = Result.Err(RuntimeError("Failed to process integrated efficiency!"))
 
-    def load(self, instantaneous_efficiency_result) -> tuple[FileLoader]:
+        return instantaneous_efficiency_result, integrated_efficiency_result
+
+    def load(self, instantaneous_efficiency_result, integrated_efficiency_result) -> tuple[FileLoader, FileLoader]:
         instantaneous_efficiency_file = File(
             canonical_path=CanonicalPath(
                 origin=self.context.title,
@@ -71,13 +84,29 @@ class EfficiencyStage(Stage):
                 name="InstantaneousEfficiency",
             ),
             file_type=FileType.TimeSeries,
-            data=instantaneous_efficiency_result.unwrap() if instantaneous_efficiency_result else None
+            data=instantaneous_efficiency_result.unwrap() if instantaneous_efficiency_result else None,
+            description="" # TODO
+        )
+
+        integrated_efficiency_file = File(
+            canonical_path=CanonicalPath(
+                origin=self.context.title,
+                event=self.event_name,
+                source=self.get_stage_name(),
+                name="IntegratedEfficiency",
+            ),
+            file_type=FileType.TimeSeries,
+            data=integrated_efficiency_result.unwrap() if integrated_efficiency_result else None,
+            description=""  # TODO
         )
 
         instantaneous_efficiency_loader = self.context.data_source.store(instantaneous_efficiency_file)
         self.logger.info(f"Successfully loaded InstantaneousEfficiency!")
 
-        return (instantaneous_efficiency_loader,)
+        integrated_efficiency_loader = self.context.data_source.store(integrated_efficiency_file)
+        self.logger.info(f"Successfully loaded IntegratedEfficiency!")
+
+        return instantaneous_efficiency_loader, integrated_efficiency_loader
 
 
 stage_registry.register_stage(EfficiencyStage.get_stage_name(), EfficiencyStage)
