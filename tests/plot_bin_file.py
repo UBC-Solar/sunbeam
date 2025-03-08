c39763d21dfea338494d6c72e5292b00112a2a6a
Jonah Lee <jonahjacqueslee@gmail.com>
2025-03-08 15:26:36 -0800

    add labels

--- tests/plot_bin_file.py
+++ tests/plot_bin_file.py
@@ -6,4 +6,7 @@ with open(os.path.join(os.getcwd(), 'EfficiencyLapDist.bin'), 'rb') as f:
     data = dill.load(f).data
 
 plt.plot(data)
+plt.title('EfficiencyLapDist.bin - FSGP 2024 Day 1')
+plt.xlabel('Lap Index, Day 1')
+plt.ylabel('Efficiency, J/m')
 plt.show()
