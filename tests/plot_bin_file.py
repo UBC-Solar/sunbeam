dce3219d0d5b1f066880a61c2f15f85a30b7211a
Jonah Lee <jonahjacqueslee@gmail.com>
2025-03-08 16:01:44 -0800

    if name == main

--- tests/plot_bin_file.py
+++ tests/plot_bin_file.py
@@ -2,11 +2,12 @@ import matplotlib.pyplot as plt
 import dill
 import os
 
-with open(os.path.join(os.getcwd(), 'EfficiencyLapDist.bin'), 'rb') as f:
-    data = dill.load(f).data
+if __name__ == '__main__':
+    with open(os.path.join(os.getcwd(), 'EfficiencyLapDist.bin'), 'rb') as f:
+        data = dill.load(f).data
 
-plt.plot(data)
-plt.title('EfficiencyLapDist.bin - FSGP 2024 Day 1')
-plt.xlabel('Lap Index, Day 1')
-plt.ylabel('Efficiency, J/m')
-plt.show()
+    plt.plot(data)
+    plt.title('EfficiencyLapDist.bin - FSGP 2024 Day 1')
+    plt.xlabel('Lap Index, Day 1')
+    plt.ylabel('Efficiency, J/m')
+    plt.show()
