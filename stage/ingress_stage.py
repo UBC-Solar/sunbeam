c95d5e00f321c0d4d2eb003f6d3cafc440d16b9e
Joshua Riefman <joshuariefman@gmail.com>
2025-05-24 12:09:02 -0700

    Unpack final result tuple

--- stage/ingress_stage.py
+++ stage/ingress_stage.py
@@ -35,7 +35,8 @@ class IngressStage(Stage):
         :param events: a list of n Event models specifying how the raw data should be temporally partitioned
         :return: a dictionary which can be indexed first by event, then by target name.
         """
-        return cast(Dict[str, Dict[str, FileLoader]], super().run(self, targets, events))
+        ingress_dict, = super().run(self, targets, events)
+        return cast(Dict[str, Dict[str, FileLoader]], ingress_dict)
 
     def __init__(self, config: DataSourceConfig):
         super().__init__()
