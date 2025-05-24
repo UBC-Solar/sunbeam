a6b595106bc1d951079fd43d2025907555c5d6a3
Jonah Lee <jonahjacqueslee@gmail.com>
2025-05-24 14:57:53 -0700

    update realtime event and plot some weather data

--- tests/plot_bin_file.py
+++ tests/plot_bin_file.py
@@ -1,13 +1,21 @@
 import matplotlib.pyplot as plt
 import dill
 import os
+from data_tools.collections import TimeSeries
 
 if __name__ == '__main__':
-    with open(os.path.join(os.getcwd(), 'EfficiencyLapDist.bin'), 'rb') as f:
-        data = dill.load(f).data
 
-    plt.plot(data)
-    plt.title('EfficiencyLapDist.bin - FSGP 2024 Day 1')
-    plt.xlabel('Lap Index, Day 1')
-    plt.ylabel('Efficiency, J/m')
+    things_to_plot = [
+        'dni.bin', 'ghi.bin', 'dhi.bin',
+    ]
+
+    data = []
+    for thing in things_to_plot:
+        with open(os.path.join(os.getcwd(), 'fs_data', 'pipeline', 'realtime', 'weather', thing), 'rb') as f:
+            data.append(dill.load(f).data)
+
+    for thing, ts in zip(things_to_plot, data):
+        plt.plot(ts.datetime_x_axis, ts, label=thing)
+    plt.legend()
+    plt.title(r'Solcast GHI, DHI, and DNI. GHI $\approx$ $\text{DHI + DNI} \cos(\theta)$')
     plt.show()
