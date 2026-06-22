2c960e7f91e7366b893153ccc590ccb2b70542af
ricd99 <ryanhuang6391@gmail.com>
2026-06-22 12:45:43 -0700

    initial implementation of energy from integrated power stage

---
+++ stage/v3_0/energy_from_integrated_power.py
@@ -0,0 +1,27 @@
+from data_tools.localization import CanonicalName
+from state.frame import Frame, FrameView
+from typing import ClassVar
+from stage.stage import Stage
+
+class EnergyFromIntegratedPower(Stage):
+    stage_name: ClassVar[str] = "EnergyFromIntegratedPower"
+    inputs: ClassVar[list[str]] = [CanonicalName.IntegratedPackPower, CanonicalName.EnergyVOLExtrapolated]
+    outputs: ClassVar[list[str]] = [CanonicalName.EnergyFromIntegratedPower]
+    frequency: ClassVar[float] = 5
+
+    def __init__(self):
+        self.initial_energy = None
+
+    def run(self, input_frame: FrameView) -> Frame:
+        new_frame = Frame.from_view(input_frame)
+        ipp = input_frame.read(CanonicalName.IntegratedPackPower)
+        eve = input_frame.read(CanonicalName.EnergyVOLExtrapolated)
+
+        if self.initial_energy is None:
+            self.initial_energy = eve
+        
+        current_energy = self.initial_energy - ipp
+
+        new_frame.write(CanonicalName.EnergyFromIntegratedPower, current_energy)
+
+        return new_frame
