61503df0e6695918fbdfd7d8315ba48648db071d
MaroStar <marwan.negm@outlook.com>
2026-09-05 11:35:38 -0700

    Replaced random number in stage names with a UUID

--- stage/v3_0/offline_ingress.py
+++ stage/v3_0/offline_ingress.py
@@ -5,7 +5,7 @@ from state.frame import Frame, FrameView
 from datetime import timezone
 from typing import ClassVar
 from stage.stage import Stage
-import random
+import uuid
 
 
 class OfflineIngress(Stage):
@@ -17,7 +17,7 @@ class OfflineIngress(Stage):
         self._frequency = 0
         self._start_date = event_start_date
         self._end_date = event_end_date
-        self._stage_name = f"Offline_Ingress{random.randint(1, 1000)}"
+        self._stage_name = f"Offline_Ingress{uuid.uuid4()}"
 
         self._localized_output_signals = []
         self._localized_signal_to_signal = {}
